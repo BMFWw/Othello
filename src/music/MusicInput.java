5083808e4973cd7f5b16977585ccd1b865f17354
BMFWw <95680085+BMFWw@users.noreply.github.com>
2021-12-27 13:24:06 +0800

    Add files via upload

---
+++ src/music/MusicInput.java
@@ -0,0 +1,8 @@
+package music;
+
+public class MusicInput {
+    public static MusicPlayer player = new MusicPlayer("CHESS\\src\\music\\bgm.wav");//创建音乐路径
+    public static MusicPlayer boardPlayer = new MusicPlayer("CHESS\\src\\music\\chessBoardBgm.wav");//创建音乐路径
+    public static MusicPlayer easyBoardPlayer = new MusicPlayer("CHESS\\src\\music\\bgmEasy.wav");//创建音乐路径
+    public static MusicPlayer click = new MusicPlayer("CHESS\\src\\music\\click.wav");//创建音乐路径
+}
